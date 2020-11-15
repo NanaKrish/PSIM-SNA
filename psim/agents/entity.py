5e62374475e116c4addbf9c74264ec819a849611
KrishnanSG <krishsg525@gmail.com>
2020-11-15 17:25:35 +0530

    Add helpers to load user data

--- psim/agents/entity.py
+++ psim/agents/entity.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from math import sqrt
 
 
 class Location:
@@ -12,17 +13,17 @@ class Location:
     @staticmethod
     def calculate_distance(loc1, loc2) -> float:
         if isinstance(loc1, Location) and isinstance(loc2, Location):
-            # TODO: Implement distance b/w 2 points algo
-            pass
+            return sqrt((loc1.x - loc2.x) ** 2 + (loc1.y - loc2.y) ** 2)
         else:
             raise Exception("Requires 2 location params, to compute the distance b/w them")
 
 
 class Person:
-    def __init__(self, name, age: int, location: Location):
+    def __init__(self, name, age: int, location: Location, health_condition: float = 100.0):
         self.age = age
         self.name = name
         self.location = location
+        self.health_condition = health_condition
 
     def __repr__(self):
         return repr(self.name)
--- psim/network/graph.py
+++ psim/network/graph.py
@@ -1,5 +1,18 @@
+from typing import Iterable
+
 import networkx as nx
 
+from psim.agents.entity import Person
 
-class InfectionGraph(nx.DiGraph):
-    pass
+
+class InfectionGraph:
+
+    def __init__(self, population: Iterable[Person]):
+        self.__population = population
+        self.__graph = self.__construct_initial_graph()
+
+    def __construct_initial_graph(self) -> nx.DiGraph:
+        """
+        Initializes the infection graph
+        """
+        pass
