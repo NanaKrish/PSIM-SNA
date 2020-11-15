ed8efb8a78d2ef8742919528cc004846eda3c23c
KrishnanSG <krishsg525@gmail.com>
2020-11-15 22:40:23 +0530

    entity: Add population class to handle collection of Persons

--- psim/agents/entity.py
+++ psim/agents/entity.py
@@ -1,5 +1,19 @@
 from abc import ABC, abstractmethod
 from math import sqrt
+from typing import List, Generator, Any
+
+import numpy as np
+
+
+class Disease(ABC):
+    def __init__(self, infectivity: float, infection_radius: float, infection_type: str):
+        self._infectivity = infectivity
+        self._infection_radius = infection_radius
+        self._infection_type = infection_type
+
+    @abstractmethod
+    def infection_function(self, *args):
+        pass
 
 
 class Location:
@@ -15,27 +29,71 @@ class Location:
         if isinstance(loc1, Location) and isinstance(loc2, Location):
             return sqrt((loc1.x - loc2.x) ** 2 + (loc1.y - loc2.y) ** 2)
         else:
-            raise Exception("Requires 2 location params, to compute the distance b/w them")
+            raise Exception(
+                f"Requires 2 location params, to compute the distance b/w them. Got {type(loc1)} & {type(loc2)}"
+            )
+
+    @staticmethod
+    def calculate_distance_matrix(population) -> np.ndarray:
+        if not isinstance(population, Population):
+            raise Exception(f"Expected population as param got {type(population)}")
+        matrix = np.zeros((population.size(), population.size()))
+
+        for i, p1 in enumerate(population.get_people()):
+            for j, p2 in enumerate(population.get_people()):
+                if i < j:
+                    matrix[i][j] = Location.calculate_distance(p1.location, p2.location)
+        return matrix
 
 
 class Person:
-    def __init__(self, name, age: int, location: Location, health_condition: float = 100.0):
-        self.age = age
+    def __init__(self, name: str, age: int, location: Location, healthy: bool = True):
         self.name = name
+        self.age = age
         self.location = location
-        self.health_condition = health_condition
+        self.healthy = healthy
 
     def __repr__(self):
         return repr(self.name)
 
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
 
-class Disease(ABC):
 
-    def __init__(self, infectivity: float, infection_radius: float, infection_type: str):
-        self._infectivity = infectivity
-        self._infection_radius = infection_radius
-        self._infection_type = infection_type
+class Population:
+    def __init__(self, data: Generator[Person, None, Any]):
+        self.__population = self.__construct_population(data)
 
-    @abstractmethod
-    def infection_function(self, *args, **kwargs):
-        pass
+    @staticmethod
+    def __construct_population(data: Generator[Person, None, Any]):
+        """General Note: This method can be optimised later if required to reduce memory usage for large datasets"""
+        return [x for x in data]
+
+    def get_people(self) -> List[Person]:
+        return self.__population
+
+    def size(self):
+        return len(self.__population)
+
+    def get_population_status(self, raw=False):
+        """
+        Returns the health status of the population
+        :param raw: If True, returns dict of people grouped into healthy and infected
+        """
+        if raw:
+            status = {
+                "healthy": [],
+                "infected": [],
+            }
+        else:
+            status = {
+                "healthy": 0,
+                "infected": 0,
+            }
+        for person in self.get_people():
+            key = "healthy" if person.healthy else "infected"
+            if raw:
+                status[key].append(person)
+            else:
+                status[key] += 1
+        return status
--- psim/simulator.py
+++ psim/simulator.py
@@ -1,2 +1,17 @@
-class PSIMSimulator:
-    pass
+from collections import defaultdict
+
+from psim.agents.cem import BaseCEM
+from psim.agents.entity import Disease, Population
+
+
+class PSIMModel:
+    def __init__(self, population: Population, disease: Disease):
+        self.population = population
+        self.disease = disease
+        self.cems = defaultdict(list)
+
+    def register_cem(self, target_week: int, cem: BaseCEM):
+        self.cems[target_week].append(cem)
+
+    def simulate(self):
+        pass
