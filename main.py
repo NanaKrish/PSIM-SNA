d3b99a4725de24761f3ceb2b702653f0b84567b9
Sivagiri Visakan <siva.g.visakan@gmail.com>
2020-11-19 02:50:10 +0530

    Add a basic driver program for the simulator.

---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+
+import dash
+import dash_cytoscape as cyto
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from psim.agents.entity import Population
+from psim.agents.infectors import COVID19
+from psim.simulator import PSIMModel
+from psim.utils import helpers
+
+app = dash.Dash(__name__)
+
+CONFIG = {
+    'DEBUG': True,
+    'DATA_DIR':  os.path.join(os.path.abspath(os.path.dirname(__file__)), 'psim/data')
+}
+
+POPULATION_DATA_FILE = os.path.join(CONFIG['DATA_DIR'], 'population.csv')
+
+people = helpers.load_user_data(POPULATION_DATA_FILE)
+population = Population(people)
+sim = PSIMModel(population, COVID19)
+
+
+app.layout = html.Div([
+    cyto.Cytoscape(
+        id='cytoscape-population-graph',
+        layout={'name': 'circle'},
+        style={'width': '80%', 'height': '600px'},
+        elements = helpers.get_cytoscape_elts(sim.population.get_graph()),
+        stylesheet=[
+        {
+            'selector': '[infected = "true"]',
+            'style': {
+                'background-color': '#ff6e66',
+            }
+        },
+        {
+            'selector': 'node',
+            'style': {
+                'label': 'data(id)',
+            }
+        },
+        {
+            'selector': '[infected = "false"]',
+            'style': {
+                'background-color': '#63f2a1',
+            }
+        }
+    ]
+    ),
+    html.Div([
+    html.Button('Advance', id='btn-advance-next', n_clicks_timestamp=0),
+])
+])
+
+
+@app.callback(Output('cytoscape-population-graph', 'elements'),
+              [Input('btn-advance-next', 'n_clicks_timestamp')])
+def advance(btn_advance):
+    sim.simulate_one_step()
+    return helpers.get_cytoscape_elts(sim.population.get_graph())
+
+def main():
+    app.run_server(debug=CONFIG['DEBUG'], use_reloader=CONFIG['DEBUG'])
+
+if __name__ == '__main__':
+    main()
--- psim/utils/helpers.py
+++ psim/utils/helpers.py
@@ -1,6 +1,9 @@
 import csv
 from pathlib import Path
 
+import networkx
+from networkx.readwrite import json_graph
+
 from psim.agents.entity import Person, Location
 
 
@@ -11,3 +14,31 @@ def load_user_data(file_path: str):
         for data in reader:
             yield Person(data[0], int(data[1]), Location(float(data[2]), float(data[3])))
 
+
+def get_cytoscape_elts(G: networkx.Graph):
+    """
+    Returns the elements as required in a format for dash-cytoscape
+    """
+    cjs = json_graph.cytoscape_data(G)
+    elements = cjs['elements']['nodes'] + cjs['elements']['edges']
+
+    for elt in elements:
+        d = elt['data']
+        # networkx sets the value attribute to the type passed when creating the 
+        # node. It may not be passable as json, which will create errors when serialising
+        # as JSON. So we replace it
+        person = d.pop('value', '')
+        if isinstance(person, Person) and not person.healthy :
+            d['infected'] = 'true'
+        else: 
+            d['infected'] = 'false'
+
+        d['label'] = str(person)
+
+        # Do the same as above if it is an edge
+        if d.get('source', None):
+            d['source'] = str(d['source'])
+
+        if d.get('target', None):
+            d['target'] = str(d['target'])
+    return elements
